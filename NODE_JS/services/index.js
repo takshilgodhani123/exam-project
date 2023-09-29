5fca4a94ec7cae66342f8327512ed4d1db527d35
takshilgodhani123 <140573613+takshilgodhani123@users.noreply.github.com>
2023-09-29 08:20:19 +0530

    Add files via upload

---
+++ NODE_JS/services/index.js
@@ -0,0 +1,5 @@
+module.exports.userService = require("./user.service");
+module.exports.tokenService = require("./token.service");
+module.exports.tokenService = require("./token.service");
+module.exports.categoryService = require("./category.service");
+module.exports.productService = require("./product.service");
